61ed6d6ee6d5defd56abbc4542495ef91e569cb8
Nicolas Lépinay <nicolas@nosoft.io>
2025-09-27 00:09:35 +0200

    FIX Errors

--- tools/generate_unified_icons.dart
+++ tools/generate_unified_icons.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7be0dac5828ec0b1535ecaed57c5ca3e91371f00880c5ff74c6ac4cf3d5851c
-size 17484
+oid sha256:e2d59d08516c73ef813695ed9111501b3f7065c48211124c7300ac4694c2d7d0
+size 17399
